e8137f34a0f07542ff2df66b909f32a1f4938e5a
LeeKiHwan <s22049@gsm.hs.kr>
2024-08-03 17:06:35 +0900

    :sparkles: :: 게임 종료 버튼 구현

---
+++ Assets/01_Scripts/Menu/GameQuitUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameQuitUI : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameQuitUILayer;
+    [SerializeField] private Button _gameQuitButton;
+    [SerializeField] private Button _gameQuitCancelButton;
+
+    private void Awake()
+    {
+        _gameQuitButton.onClick.AddListener(() => Application.Quit());
+        _gameQuitCancelButton.onClick.AddListener(() => _gameQuitUILayer.SetActive(false));
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _gameQuitUILayer.SetActive(true);
+        }
+    }
+}
