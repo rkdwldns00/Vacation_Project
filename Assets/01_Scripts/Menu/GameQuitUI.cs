b95cae26aa2feb633bc7e27bb82e929a46c9fbc5
LeeKiHwan <s22049@gsm.hs.kr>
2024-08-04 20:02:41 +0900

    :art: :: 게임 종료 UI 활성화 버튼 구현

--- Assets/01_Scripts/Menu/GameQuitUI.cs
+++ Assets/01_Scripts/Menu/GameQuitUI.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class GameQuitUI : MonoBehaviour
 {
     [SerializeField] private GameObject _gameQuitUILayer;
+    [SerializeField] private Button _gameQuitUIActiveButton;
     [SerializeField] private Button _gameQuitButton;
     [SerializeField] private Button _gameQuitCancelButton;
 
     private void Awake()
     {
+        _gameQuitUIActiveButton.onClick.AddListener(() => _gameQuitUILayer.SetActive(true));
         _gameQuitButton.onClick.AddListener(() => Application.Quit());
         _gameQuitCancelButton.onClick.AddListener(() => _gameQuitUILayer.SetActive(false));
     }
