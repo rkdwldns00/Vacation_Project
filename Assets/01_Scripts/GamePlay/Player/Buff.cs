e4affa71ef4d677520f87329d9b0a845b4eb8fd2
LeeKiHwan <s22049@gsm.hs.kr>
2024-08-07 00:34:25 +0900

    :sparkles: :: BuffSystem, Buff 클래스 구현

---
+++ Assets/01_Scripts/GamePlay/Player/Buff.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Buff
+{
+    public abstract void StartBuff();
+    public abstract void UpdateBuff();
+    public abstract void EndBuff();
+}
+
---
+++ Assets/01_Scripts/GamePlay/Player/BuffSystem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffSystem : MonoBehaviour
+{
+    private List<Buff> _buffs = new List<Buff>();
+
+    public void AddBuff(Buff buff)
+    {
+        _buffs.Add(buff);
+        buff.StartBuff();
+    }
+
+    private void Update()
+    {
+        foreach (Buff buff in _buffs)
+        {
+            buff.UpdateBuff();
+        }
+    }
+}
