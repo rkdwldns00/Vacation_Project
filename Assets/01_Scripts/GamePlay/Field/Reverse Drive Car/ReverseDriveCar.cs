d3b31a0a4fd5b1a7c1683cc9c8be1ad352b0bc63
LeeKiHwan <s22049@gsm.hs.kr>
2024-08-08 23:53:08 +0900

    :sparkles: :: 역주행 차량 구현

---
+++ Assets/01_Scripts/GamePlay/Field/Reverse Drive Car/ReverseDriveCar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseDriveCar : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _carModels;
+    [SerializeField] private float _minMoveSpeed;
+    [SerializeField] private float _maxMoveSpeed;
+    [SerializeField] private float _startTime;
+
+    private float _moveSpeed;
+
+    private void Awake()
+    {
+        int randCarIndex = Random.Range(0, _carModels.Length);
+        _carModels[randCarIndex].SetActive(true);
+
+        _moveSpeed = Random.Range(_minMoveSpeed, _maxMoveSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Player.Instance != null && transform.position.z <= Player.Instance.transform.position.z + _startTime * Player.Instance.MoveSpeed)
+        {
+            transform.Translate(Vector3.back * _moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
