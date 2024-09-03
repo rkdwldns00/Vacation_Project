af0f185c250c5232d04d4091218e41447cc84016
LeeKiHwan <s22049@gsm.hs.kr>
2024-09-03 10:23:43 +0900

    :art: :: SoundManager 클래스 수정

--- Assets/01_Scripts/SoundManager.cs
+++ Assets/01_Scripts/SoundManager.cs
@@ -4,29 +4,60 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    private static SoundManager instance;
-    public static SoundManager Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                GameObject g = new GameObject("Sound Manager");
-                instance = g.AddComponent<SoundManager>();
-            }
-            return instance;
-        }
-    }
+    private static SoundManager Instance;
 
-    private AudioSource _audioSource;
+    //public static SoundManager Instance
+    //{
+    //    get
+    //    {
+    //        if (instance == null)
+    //        {
+    //            GameObject g = new GameObject("Sound Manager");
+    //            DontDestroyOnLoad(g);
+    //            instance = g.AddComponent<SoundManager>();
+    //        }
+    //        return instance;
+    //    }
+    //}
+
+    [Header("Audio Source")]
+    [SerializeField] private AudioSource _bgmAudioSource;
+    [SerializeField] private AudioSource _sfxAudioSource;
+
+    [Header("Audio Clip")]
+    public AudioClip MenuBgm;
+    public AudioClip GameBgm;
 
     private void Awake()
     {
-        _audioSource = gameObject.AddComponent<AudioSource>();
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    public void PlayClip(AudioClip clip)
+    public void PlaySount(AudioClip audioClip, SoundType soundType = SoundType.SFX)
     {
-        _audioSource.PlayOneShot(clip);
+        if (soundType == SoundType.SFX)
+        {
+            _sfxAudioSource.PlayOneShot(audioClip);
+        }
+        else if (soundType == SoundType.BGM)
+        {
+            if (_bgmAudioSource.isPlaying) _bgmAudioSource.Stop();
+            _bgmAudioSource.clip = audioClip;
+            _bgmAudioSource.Play();
+        }
     }
+}
+
+public enum SoundType
+{
+    BGM,
+    SFX
 }
