aad01ee79955c194e5493e4bf1ae235dabd750c7
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-10-16 17:21:01 +0900

    :sparkles: :: 날짜변경 이벤트 구현

---
+++ Assets/01_Scripts/Menu/RealtimeEventHandler.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealtimeEventHandler : MonoBehaviour
+{
+    private static RealtimeEventHandler _instance;
+    public static RealtimeEventHandler Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<RealtimeEventHandler>();
+                if (_instance == null)
+                {
+                    GameObject g = new GameObject("RealtimeEventHandler");
+                    _instance = g.AddComponent<RealtimeEventHandler>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public event Action OnChangeDay;
+    public event Action OnFirstLogin;
+
+    public int IntervalWithLastLoginDay { get; private set; }
+    public bool isFirstLogin { get; private set; }
+
+    private int today;
+
+    private void Awake()
+    {
+        today = TicksToDay(DateTime.Now.Ticks);
+        int lastLoginDay = PlayerPrefs.GetInt("lastLoginDay");
+        isFirstLogin = lastLoginDay == 0;
+        IntervalWithLastLoginDay = today - lastLoginDay;
+    }
+
+    private void Start()
+    {
+        if (TutorialManager.isActive) return;
+
+        PlayerPrefs.SetInt("lastLoginDay", today);
+        if (isFirstLogin)
+        {
+            OnFirstLogin?.Invoke();
+        }
+        if (IntervalWithLastLoginDay > 0)
+        {
+            OnChangeDay?.Invoke();
+        }
+    }
+
+    private int TicksToDay(long tick)
+    {
+        return (int)(tick / 10000000 / 60 / 60 / 24);
+    }
+}
