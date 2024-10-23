f41c36d4adb7ed093385a4597468417c44c542a2
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-10-23 17:35:35 +0900

    :bug: :: 가챠 보상 오탈자 수정

--- Assets/01_Scripts/Menu/Gacha System/Gacha Rewards/TicketReward.cs
+++ Assets/01_Scripts/Menu/Gacha System/Gacha Rewards/TicketReward.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "티켓보상", menuName = "가챠/가챠 보상/티켓", order = int.MinValue)]
 public class TicketReward : GachaReward
 {
-    public override string GetName(int rate) => rate + " 티켓";
+    public override string GetName(int rate) => "티켓 " + rate.ToString() + "개";
 
     public override void GetReward(int rate)
     {
