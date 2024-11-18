bd7bde333fc8bb5a7042e2f55826904c0ba7dd55
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-11-18 16:53:08 +0900

    ✨ :: 최대 체력 차량 리워크

--- Assets/01_Scripts/GamePlay/Player/Ablility/MaxHealthPlayer.cs
+++ Assets/01_Scripts/GamePlay/Player/Ablility/MaxHealthPlayer.cs
@@ -4,23 +4,48 @@ using UnityEngine;
 
 public class MaxHealthPlayer : Player
 {
-    public override int MaxLevel => 2;
+    public override int MaxLevel => 5;
     public override int UpgradeCost => 500;
-    public override int UnlockCost => 100;
+    public override int UnlockCost => 150;
     public override string CarInfo
     {
         get
         {
             if (PlayerLevel == 0)
             {
-                return string.Format("더욱 단단해졌습니다,\n체력이 최대 {0}칸 늘어납니다.", MaxLevel);
+                return string.Format("젬을 한칸 채울 때 마다\n최대 {0}초 부스트를 얻습니다.", GetBoostTime(MaxLevel));
             }
             else
             {
-                return string.Format("더욱 단단해졌습니다,\n체력이 {0}칸 늘어납니다.", PlayerLevel);
+                return string.Format("젬을 한칸 채울 때 마다\n{0}초 부스트를 얻습니다.", GetBoostTime(PlayerLevel));
             }
         }
     }
 
-    public override int MaxHealth => base.MaxHealth + PlayerLevel;
+    [SerializeField] private BuffSystem _buffSystem;
+    [SerializeField] private float _boostSpeed;
+    private int _lastBoostGazy;
+    
+
+    protected override void Reset()
+    {
+        base.Reset();
+        _buffSystem = GetComponent<BuffSystem>();
+    }
+
+    private float GetBoostTime(int playerLevel)
+    {
+        return playerLevel / 5f;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if ((int)BoostGazy > _lastBoostGazy)
+        {
+            _buffSystem.AddBuff(new BoostBuff(_boostSpeed, GetBoostTime(PlayerLevel)));
+        }
+        _lastBoostGazy = (int)BoostGazy;
+    }
 }
